1f5e3fe0c9607912630e17d52aa1cffc94acade5
HasanCodeX <dev.hasan10@gmail.com>
2025-02-08 23:44:59 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,13 @@
+ //**
+ // COMPARISON OPERATORS
+ // 1.bigger :>
+ // 2.less:<
+ // 3.equal: ==
+ // 4.grater then equal or equal :>=
+ // 5.less then or equal:<=
+ // 6.not equal: !=
+ // 
+ // -------------------
+ // 7.and: && 
+ // 8.or: || 
+ // */
---
+++ variyable.js
@@ -0,0 +1,6 @@
+var sunglassPrice = 15;
+var subject = "eee";
+var passed = true;
+
+
+var lastExamResult = "89";
