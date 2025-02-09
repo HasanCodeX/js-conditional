263cdf4a0784762fa91f628c9d2e83be3841c825
HasanCodeX <dev.hasan10@gmail.com>
2025-02-09 23:23:25 +0600

    camparism

---
+++ comparism.js
@@ -0,0 +1,18 @@
+// console.log(5 > 10);
+
+// console.log(5 < 10); // false
+
+// console.log(5 == 10); // false
+
+// console.log(5 >= 10); // false
+
+// console.log(5 <= 10); 
+
+// console.log(5 == 10); 
+
+//-------------
+
+// var peyarPrice = 40;
+// var applePrice = 30;
+// console.log(peyarPrice > applePrice); // true
+
