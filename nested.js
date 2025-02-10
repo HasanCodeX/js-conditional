a2685bf7ad18570fc923506cd6b5a35fe18eaa82
HasanCodeX <dev.hasan10@gmail.com>
2025-02-10 15:53:41 +0600

    nested explore

---
+++ nested.js
@@ -0,0 +1,8 @@
+const money = 500;
+if (money > 300){
+    console.log('You are rich');
+} else if (money > 100){
+    console.log('You are middle class');
+} else if (money > 50){
+    console.log('You are poor');
+}
