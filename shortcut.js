1fddb1b8c89b0482fe5df516899ff2e06839c11f
HasanCodeX <dev.hasan10@gmail.com>
2025-02-10 16:40:49 +0600

    short rule explore

---
+++ shortcut.js
@@ -0,0 +1,15 @@
+const isLeader = true;
+if (isLeader == true){
+    console.log('leader asce !!!')
+}
+else{
+    console.log('leader nai !!!')
+}
+
+const ispassed = false ;
+if (ispassed == false){
+    console.log('you are failed')
+}
+else{  
+    console.log('you are passed')
+}
